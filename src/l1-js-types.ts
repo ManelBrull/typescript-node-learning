96eb7de4b4e480a750da8113d8d9edbe675ebf3f
Manel Brull <mbrull88@gmail.com>
2025-03-24 16:29:50 +0100

    Understanding types in JavaScript

---
+++ src/l1-js-types.ts
@@ -0,0 +1,24 @@
+
+const thisName = "Bob";
+console.log("Type of thisName:", typeof thisName);
+
+const age = 30;
+console.log("Type of age:", typeof age);
+
+const cool = true;
+console.log("Type of cool:", typeof cool);
+
+const dateOfBirth = new Date(1989, 10, 5);
+console.log("Type of dateOfBirth:", typeof dateOfBirth);
+
+const count = BigInt(452947234234);
+console.log("count", typeof count);
+
+const address = null;
+console.log("address", typeof address);
+
+const phone = undefined;
+console.log("phone", typeof phone);
+
+const stars = Symbol("***");
+console.log("stars", typeof stars);
